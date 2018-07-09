662fd6396532b26792b3917affe406651500de09
André Straubmeier <a.straubmeier@gmail.com>
2018-07-09 19:51:48 +0200

    Fixed tabs vs spaces

--- Assets/Scripts/HashLookup.cs
+++ Assets/Scripts/HashLookup.cs
@@ -11,23 +11,23 @@ public class HashLookup
 	{
 		using (var reader = new StringReader(text))
 		{
-		    string line;
-		    while ((line = reader.ReadLine()) != null)
-		    {
-		        var parts = line.Split(',');
+			string line;
+			while ((line = reader.ReadLine()) != null)
+			{
+				var parts = line.Split(',');
 
-		        if (parts.Length != 3) continue;
+				if (parts.Length != 3) continue;
 
-		        // 0: Unity GUID
-		        // 1: Rust UID
-		        // 2: Asset path
+				// 0: Unity GUID
+				// 1: Rust UID
+				// 2: Asset path
 
-		        var uid = uint.Parse(parts[1]);
-		        var str = parts[2];
+				var uid = uint.Parse(parts[1]);
+				var str = parts[2];
 
-		        uid2str.Add(uid, str);
-		        str2uid.Add(str, uid);
-		    }
+				uid2str.Add(uid, str);
+				str2uid.Add(str, uid);
+			}
 		}
 	}
 
